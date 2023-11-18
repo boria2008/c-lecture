58d2afd17263e72fbeca8cc5b4812f609934c609
Boris_Razor <boria2008@inbox.ru>
2023-11-18 17:24:16 +0300

    added my second project 'Hello,username!'

---
+++ example_02/Program.cs
@@ -0,0 +1,5 @@
+﻿// Создаем программу, которая запрашивает имя и выводит его в консоли с приветствием.
+Console.WriteLine ("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine ("Hello, ");
+Console.WriteLine (username);
