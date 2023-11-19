770510f127e46a90204292924d8f375ac4015667
Boris_Razor <boria2008@inbox.ru>
2023-11-19 20:05:12 +0300

    Начал писать программу особенного приветствия

---
+++ example_05_Hi/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа для особенного приветствия Бориса
+Console.WriteLine("Введите ваше имя");
+string username=Console.ReadLine();
+if(username=="Boris");
+{
+    Console.WriteLine("Приветствую тебя, внук богов!");
+}
+else
+{
+    Console.WriteLine("Здравствуйте!");
+}
