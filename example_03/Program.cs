9a6881287fac93d76509788a4faad45620b8387c
Boris_Razor <boria2008@inbox.ru>
2023-11-19 17:35:22 +0300

    Добавил в 3 пример случайный выбор чисел

--- example_03/Program.cs
+++ example_03/Program.cs
@@ -8,3 +8,11 @@ int numberC = 7;
 int numberD = 12;
 int result=numberC+numberD;
 Console.WriteLine(result);
+
+int numberE=new Random().Next(-5,20);
+Console.WriteLine(numberE);
+int numberF=new Random().Next(-5,20);
+Console.WriteLine(numberF);
+Console.WriteLine(numberE+numberF);
+
+
