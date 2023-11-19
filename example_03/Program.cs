f5c42eaca85a98f2cff997dbeab9175911c62b39
Boris_Razor <boria2008@inbox.ru>
2023-11-19 10:37:49 +0300

    added example div & sum

--- example_03/Program.cs
+++ example_03/Program.cs
@@ -1,3 +1,10 @@
 ﻿int numberA = 5;
 int numberB = 17;
 Console.WriteLine(numberA+numberB);
+
+
+
+int numberC = 7;
+int numberD = 12;
+int result=numberC+numberD;
+Console.WriteLine(result);
---
+++ example_04_div/Program.cs
@@ -0,0 +1,21 @@
+﻿// div
+
+
+double numberC = 12;
+double numberD = 5;
+Console.WriteLine(numberC/numberD);
+
+double numberA = 12;
+double numberB = 5;
+double result=numberA/numberB;
+Console.WriteLine(result);
+
+int numberE = 12;
+int numberF = 5;
+Console.WriteLine(numberE/numberF);
+
+int numberG = 12;
+int numberH = 5;
+double res=numberG/numberH;
+Console.WriteLine(res);
+
